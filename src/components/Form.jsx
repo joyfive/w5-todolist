707a390c1c6bf404ec61f0ccc4ee1a836950d2c7
joyfive <joy_five@kakao.com>
2022-10-10 23:35:24 +0900

    html, 기존소스 추가 & redux 리팩토링 시작

---
+++ src/components/Form.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+import './App.css';
+import styled from "styled-components";
+
+let number = 4;
+function Form({ setTodos, todos }) {
+  const todoItem = {
+    id: 0,
+    title: "",
+    body: "",
+    isDone: false,
+  };
+ 
+  console.log(todos);
+
+  const [todo, setTodo] = useState(todoItem);
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setTodo({ ...todo, [name]: value });
+  };
+
+  const onReset = (e) => {
+    setTodo({
+      title: "",
+      body: "",
+    });
+  };
+
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo.title.trim() === "" || todo.body.trim() === "") return;
+    setTodos([...todos, { ...todo, id: number }]);
+    setTodo(todoItem);
+    number++;
+  };
+  
+  const FormBox = styled.form`
+  width: 100%;
+  height: 40px;
+  margin: 0 0 120px 0;
+`;
+
+const InputContainer = styled.div`
+  width: 100%;
+  margin: 0 auto 0 auto;
+  padding: 0 auto 0 auto;
+`
+
+const InputBox = styled.input`
+  width: 40%;
+  height: 56px;
+  
+`
+
+const BtnBox = styled.div`
+  width: 100%;
+  margin: 0 auto 0 auto;
+  padding: 0 auto 0 auto;
+`
+
+const BtnAdd = styled.button`
+  width: 20%;
+  height: 56px;
+`
+const BtnReset = styled.button`
+  width: 20%;
+  height: 56px;
+`
+
+  return (
+    <FormBox onSubmit={onSubmit} id="add">
+      <InputContainer>
+            <InputBox
+              type="text"
+              name="title"
+              value={todo.title}
+              onChange={onChange}
+              placeholder='제목'
+            />
+            <InputBox
+              type="text"
+              name="body"
+              value={todo.body}
+              onChange={onChange}
+              className="input-txt"
+              placeholder='내용'
+            />
+        </InputContainer>
+        <BtnBox>
+          <BtnAdd type="submit" form="add" className="form-btn" >추가하기</BtnAdd>
+          <BtnReset type="button" onClick={onReset} className="form-btn">리셋하기</BtnReset>
+        </BtnBox>
+    </FormBox>
+  );
+}
+  
+
+// function getCurrentState () {}
+// export default connect(getCurrentState) (Form);
+export default Form;
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,7 +1,47 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
+import counter from "../modules/counter";
 
-const rootReducer = combineReducers({});
-const store = createStore(rootReducer);
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+
+export const addTodo = (title, body) => {
+    return {
+        type:ADD,
+        title,
+        body
+    }
+}
+
+
+export const deleteTodo = id => {
+    return {
+        type: DELETE,
+        id
+    }
+}
+
+const reducer = (state = [], action) => {
+    switch (action.type) {
+        case ADD:
+            return [{ title: action.title, body: action.body, id: Date.now() }, ...state]
+        case DELETE:
+            return state.filter(todo => todo !== action.id);
+        default:
+        return state;
+    
+        }
+};
+
+const store = createStore(reducer);
+store.subscribe()
+
+// const rootReducer = combineReducers({
+//     counter: counter,
+// });
+
+// const store = createStore(rootReducer);
 
 export default store;
