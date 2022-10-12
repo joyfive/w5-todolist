9fe73186514a36f9630b13c7324b9cf53740615f
joyfive <joy_five@kakao.com>
2022-10-12 21:54:52 +0900

    UPDATE font import

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=IBM+Plex+Sans+KR:wght@300;400;500&family=Noto+Serif+KR:wght@600;700&display=swap');
 * {
   font-family: 'Cormorant Garamond', serif;
   font-family: 'IBM Plex Sans KR', sans-serif;
